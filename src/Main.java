fa25b2c16afb9742aae78895f78714a2732b43e2
Sobh6n <sobhankhedry7297@gmail.com>
2024-05-23 20:51:27 +0330

    Working, not done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+    static int[] Floors = {0,1,2,3,4,5,6,7,8,9,10};
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x =ReachB(Floors,9,2,2,4);
+        System.out.println(x);
+    }
+
+    private static int ReachB(int[] floors, int B, int A, int u, int d) {
+        if((A>B) || (A==B)){
+            return 0;
+        }
+        if(u+A > B){
+            return 1+ ReachB(floors,B, A-d,u,d);
+        }
+        else {
+            return 1 + ReachB(floors,B,A+u , u ,d);
+        }
     }
 }
