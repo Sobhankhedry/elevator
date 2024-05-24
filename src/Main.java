090955de0d5f91f9801b8dc840767ae69ada01ef
Sobh6n <sobhankhedry7297@gmail.com>
2024-05-24 11:04:14 +0330

    working not done eeee

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,48 @@
+import java.util.Arrays;
+
 public class Main {
-    static int[] Floors = {0,1,2,3,4,5,6,7,8,9,10};
+    static int[] Floors = {0, 1, 2, 3, 4};
+    static int[] r = new int[5];
+
+
     public static void main(String[] args) {
-        int x =ReachB(Floors,9,2,2,4);
-        System.out.println(x);
+        System.out.println("min jumps : ");
+        System.out.println(ReachB(Floors, r, 4, 3, 2, 1));
+
+
+        System.out.println("");
+        System.out.println("");
+        for (int i = 0; i < r.length; i++) {
+                System.out.print(r[i] + " ");
+            System.out.println("");
+        }
     }
 
-    private static int ReachB(int[] floors, int B, int A, int u, int d) {
-        if((A>B) || (A==B)){
+    private static int ReachB(int[] Floors,int[] r, int B, int A, int u, int d) {
+        if(A==B){
+            r[A] = 1;
+            System.out.println("helloooo");
             return 0;
         }
-        if(u+A > B){
-            return 1+ ReachB(floors,B, A-d,u,d);
+        if(A>B){
+            return 10000;
+        }
+        if(A<0){
+            return 10000;
+        }
+        if(r[A] > 0){
+            return Integer.MAX_VALUE;
+        }
+        int p = 1+ ReachB(Floors,r,B,A+u,u,d);
+        int q = 1+ ReachB(Floors,r,B,A-d,u,d);
+
+        if(p<q){
+            r[A] = p;
+            return p;
         }
         else {
-            return 1 + ReachB(floors,B,A+u , u ,d);
+            r[A]=q;
+            return q;
         }
     }
 }
