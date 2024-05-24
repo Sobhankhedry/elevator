f2d05cc03c589af33c42413405e3c53e3dd7845d
Sobh6n <sobhankhedry7297@gmail.com>
2024-05-24 11:14:41 +0330

    done

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 public class Main {
     static int[] Floors = {0, 1, 2, 3, 4};
     static int[] r = new int[5];
+    static int[] s =new int[5];
+    static int navigate;
 
 
     public static void main(String[] args) {
@@ -11,17 +13,28 @@ public class Main {
 
 
         System.out.println("");
-        System.out.println("");
+        System.out.println("r:");
         for (int i = 0; i < r.length; i++) {
                 System.out.print(r[i] + " ");
-            System.out.println("");
+
+        }
+        System.out.println("");
+        System.out.println("S:");
+        for (int i = 0; i < r.length; i++) {
+            if (s[i]!=0)
+            System.out.print(s[i] + " ");
+
         }
     }
 
     private static int ReachB(int[] Floors,int[] r, int B, int A, int u, int d) {
+        if(r[B] > 0){
+            return 0;
+        }
         if(A==B){
             r[A] = 1;
-            System.out.println("helloooo");
+            s[navigate]=A;
+            System.out.println("the A" + A);
             return 0;
         }
         if(A>B){
@@ -38,10 +51,14 @@ public class Main {
 
         if(p<q){
             r[A] = p;
+            s[navigate]= A;
+            navigate++;
             return p;
         }
         else {
             r[A]=q;
+            s[navigate]=A;
+            navigate++;
             return q;
         }
     }
