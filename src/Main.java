f434bbb12c19ebbf4ba852a18a9ddf268d9807c1
Sobh6n <sobhankhedry7297@gmail.com>
2024-05-24 12:38:29 +0330

    done bot for not reaching has bug

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,102 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
-    static int[] Floors = {0, 1, 2, 3, 4};
-    static int[] r = new int[5];
-    static int[] s =new int[5];
+    static int[] Floors = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    static int[] r = new int[11];
+    static int[] s =new int[11];
+    static int n = s.length;
     static int navigate;
-
+    static int flag = -1;
 
     public static void main(String[] args) {
-        System.out.println("min jumps : ");
-        System.out.println(ReachB(Floors, r, 4, 3, 2, 1));
 
+        ReachB(Floors, r, 10, 2, 7, 3);
 
-        System.out.println("");
-        System.out.println("r:");
+        int x = 0;
+
         for (int i = 0; i < r.length; i++) {
-                System.out.print(r[i] + " ");
+
+            System.out.print(r[i]+" ");
+        }
+
+        System.out.println(" ");
+        for (int i = 0; i < r.length; i++) {
+            if(r[i]>0 & r[i] != 0 & r[i]!= Integer.MAX_VALUE){
+                x = x+ r[i];
+            }
 
         }
-        System.out.println("");
-        System.out.println("S:");
-        for (int i = 0; i < r.length; i++) {
-            if (s[i]!=0)
-            System.out.print(s[i] + " ");
-
+        System.out.println("min jumps : " + x );
+        if(x==0){
+            System.out.println("Sorry can't reach B");
         }
+        if(x!=0){
+            System.out.println("the route : ");
+            s = Reverse(s);
+            for (int i = 0; i < s.length; i++) {
+                if (s[i]!=0){
+                    System.out.print(s[i]+ " ");
+                }
+
+            }
+        }
+
+    }
+
+    private static int[] Reverse(int[] s) {
+        int[] b = new int[n];
+        int j = n;
+        for (int i = 0; i < n; i++) {
+            b[j - 1] = s[i];
+            j = j - 1;
+        }
+
+        // printing the reversed array
+        return b;
     }
 
     private static int ReachB(int[] Floors,int[] r, int B, int A, int u, int d) {
+        if(flag == 0){
+            return  Integer.MAX_VALUE;
+        }
         if(r[B] > 0){
             return 0;
         }
         if(A==B){
-            r[A] = 1;
-            s[navigate]=A;
-            System.out.println("the A" + A);
+            r[A] = Integer.MAX_VALUE;
+            s[navigate]=B;
+            navigate++;
+            //System.out.println("the A" + A);
             return 0;
         }
         if(A>B){
-            return 10000;
-        }
-        if(A<0){
-            return 10000;
-        }
-        if(r[A] > 0){
+            r[A-u]=Integer.MAX_VALUE;
+            //flag=0;
             return Integer.MAX_VALUE;
         }
-        int p = 1+ ReachB(Floors,r,B,A+u,u,d);
-        int q = 1+ ReachB(Floors,r,B,A-d,u,d);
+        if(A<0){
+            r[A+d] = Integer.MAX_VALUE;
+            //flag=0;
+            return Integer.MAX_VALUE;
+        }
+        if(r[A] > 0){
+
+            return Integer.MAX_VALUE;
+        }
+        int p =  ReachB(Floors,r,B,A+u,u,d);
+        int q =  ReachB(Floors,r,B,A-d,u,d);
 
         if(p<q){
-            r[A] = p;
-            s[navigate]= A;
+            r[A] = 1+ p;
+            s[navigate]=A;
             navigate++;
             return p;
         }
         else {
-            r[A]=q;
+            r[A]= 1 + q;
             s[navigate]=A;
-            navigate++;
+            navigate ++;
             return q;
         }
     }
